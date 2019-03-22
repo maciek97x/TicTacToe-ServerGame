21f92e3c4295093f1f77b60c330418e1a60df62f
maciek97x <maciek97x@o2.pl>
2019-03-22 10:01:48 +0100

    update

--- client.py
+++ client.py
@@ -4,15 +4,15 @@ import time
 import pygame
 from pygame.locals import *
 from threading import Thread
-from mygui import *
+import mygui
 
 
 import os
 
+# creating socket
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "127.0.0.1"
 port = 8888
-
 
 # window size
 window_width = 900
@@ -22,28 +22,43 @@ graph_size = 600
 # initializing window
 pygame.init()
 window = pygame.display.set_mode((window_width, window_height), 0, 32)
-pygame.display.set_caption('Interpolacja wielomianem i splajnem')
+pygame.display.set_caption('Tic Tac Toe')
 icon = pygame.Surface((1, 1))
 icon.fill((255, 255, 255))
 pygame.display.set_icon(icon)
 
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+    
 def handle_connection(socket):
     while True:
         msg_from_server = socket.recv(1024).decode("utf8")
         
-
+'''
 try:
     soc.connect((host, port))
 except:
     print("Connection error")
     sys.exit()
+'''
+        
+menu = 0
 
+gui = mygui.GUI(window)
+gui.add(mygui.TextBox(window, (60, 60), (100, 20), 'insert your nick'))
 
 # main loop
 while True:
-    # handling events
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            terminate()
-
+    if menu == 0:
+        while True:
+            # handling events
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    terminate()
+                gui.handle_event(event)
+                
+            window.fill(mygui.colors['white'])
+            gui.draw()
+            pygame.display.update()
--- mygui.py
+++ mygui.py
@@ -1,4 +1,4 @@
-import pygame, sys, os, time
+import pygame, sys, os, time, string
 import numpy as np
 from pygame.locals import *
 
@@ -8,19 +8,20 @@ colors = {'white':          (255, 255, 255),
           'red':            (255, 127, 127),
           'blue':           (127, 127, 255)}
 
-def terminate():
-    pygame.quit()
-    sys.exit()
     
-def draw_text(text, size, color, surface, position, allign=''):
+def draw_text(text, size, color, surface, position, align=''):
     # function for drawing text on screen
     font = pygame.font.SysFont("consolasms", size)
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
-    if allign == 'lb':
+    if align == 'bottomleft':
         textrect.bottomleft = position
-    elif allign == 'lt':
+    elif align == 'topleft':
         textrect.topleft = position
+    elif align == 'bottomright':
+        textrect.bottomright = position
+    elif align == 'topright':
+        textrect.topright = position
     else:
         textrect.center = position
     surface.blit(textobj, textrect)
@@ -34,9 +35,9 @@ class GUI:
         for element in self.__elements:
             element.draw()
             
-    def handle_click(self, event):
+    def handle_event(self, event):
         for element in self.__elements:
-            element.handle_click(event)
+            element.handle_event(event)
     
     def add(self, element):
         if isinstance(element, GUIElement):
@@ -51,20 +52,28 @@ class GUIElement:
         self.__size = size
         
     @property
+    def window(self):
+        return self.__window
+    
+    @window.setter
+    def window(self, value):
+        self.__window = value
+        
+    @property
     def pos(self):
         return self.__pos
 
     @pos.setter
-    def pos(self, pos):
-        self.__pos = pos
+    def pos(self, value):
+        self.__pos = value
         
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, size):
-        self.__size = size
+    def size(self, value):
+        self.__size = value
     
 
 class Button(GUIElement):
@@ -75,13 +84,15 @@ class Button(GUIElement):
 
     def draw(self):
         pygame.draw.rect(self.__window, (0, 0, 0), pygame.Rect(self.__pos, self.__size))
-        draw_text(self.__text, self.__size[0], colors['black'], self.__window,\
-                  (self.__pos[0] + self.__size[0], self.__pos[1] + self.__size[1]), 'lb')
+        text_size = self.size[1]*4//5
+        draw_text(self.__text, text_size, colors['black'], self.window,\
+                  (self.pos[0] + self.size[0]/2, self.pos[1] + self.size[1]/2))
         
-    def handle_click(self, event):
-        borders = (self.__pos[0], self.__pos[1], self.__pos[0] + self.__size[0], self.__pos[1] + self.__size[1])
-        if borders[0] <= event.__pos[0] <= borders[2] and borders[1] <= event.__pos[1] <= borders[3]:
-            pass
+    def handle_event(self, event):
+        if event.type == MOUSEBUTTONDOWN:
+            borders = (self.pos[0], self.pos[1], self.pos[0] + self.size[0], self.pos[1] + self.size[1])
+            if borders[0] <= event.pos[0] <= borders[2] and borders[1] <= event.pos[1] <= borders[3]:
+                pass
             
 class CheckBox(GUIElement):
     def __init__(self, window, pos, size, text = '', color='white', checked = True):
@@ -91,35 +102,77 @@ class CheckBox(GUIElement):
         self.__checked = checked
     
     @property
-    def is_checked(self):
+    def checked(self):
         return self.__checked
     
     @checked.setter
-    def checked(self, checked):
-        self.__checked = checked
+    def checked(self, value):
+        self.__checked = value
     
     def draw(self):
-        pygame.draw.rect(self.__window, (0, 0, 0), pygame.Rect(self.__pos, self.__size))
-        draw_text(self.__text, self.__size[0], colors['black'], self.__window,\
-                  (self.__pos[0] + self.__size[0], self.__pos[1] + self.__size[1]), 'lb')
+        pygame.draw.rect(self.__window, (0, 0, 0), pygame.Rect(self.pos, self.size))
+        text_size = self.size[1]*4//5
+        draw_text(self.__text, text_size, colors['black'], self.window,\
+                  (self.pos[0] + self.size[0], self.pos[1] + self.size[1]), 'lb')
         if self.value:
-            pygame.draw.line(self.__window, colors[self.__color], (self.__pos[0] + 5, self.__pos[1] + 5),
-                             (self.__pos[0] + self.__size[0] - 5, self.__pos[1] + self.__size[1] - 5), 5)
-            pygame.draw.line(self.__window, colors[self.__color], (self.__pos[0] + self.__size[0] - 5, self.__pos[1] + 5),
-                             (self.__pos[0] + 5, self.__pos[1] + self.__size[1] - 5), 5)
+            pygame.draw.line(self.window, colors[self.__color], (self.pos[0] + 5, self.pos[1] + 5),
+                             (self.pos[0] + self.size[0] - 5, self.pos[1] + self.size[1] - 5), 5)
+            pygame.draw.line(self.window, colors[self.__color], (self.pos[0] + self.size[0] - 5, self.pos[1] + 5),
+                             (self.pos[0] + 5, self.pos[1] + self.size[1] - 5), 5)
         
-    def handle_click(self, event):
-        borders = (self.__pos[0], self.__pos[1], self.__pos[0] + self.__size[0], self.__pos[1] + self.__size[1])
-        if borders[0] <= event.__pos[0] <= borders[2] and borders[1] <= event.__pos[1] <= borders[3]:
-            self.__checked = not self.__checked
+    def handle_event(self, event):
+        if event.type == MOUSEBUTTONDOWN:
+            borders = (self.pos[0], self.pos[1], self.pos[0] + self.size[0], self.pos[1] + self.size[1])
+            if borders[0] <= event.pos[0] <= borders[2] and borders[1] <= event.pos[1] <= borders[3]:
+                self.__checked = not self.__checked
             
         
-class TextBox:
+class TextBox(GUIElement):
     def __init__(self, window, pos, size, prompt_text=''):
         GUIElement.__init__(self, window, pos, size)
         self.__prompt_text = prompt_text
+        self.__text = ''
+        self.__is_active = False
         
+    @property
+    def text(self):
+        return self.__text
+    
+    @text.setter
+    def text(self, value):
+        self.__text = value
         
+    @property
+    def is_active(self):
+        return self.__is_active
+    
+    @is_active.setter
+    def is_active(self, value):
+        self.__is_active = value
+    
+    def draw(self):
+        pygame.draw.rect(self.window, (0, 0, 0), pygame.Rect(self.pos, self.size))
+        pygame.draw.rect(self.window, (255, 255, 255), pygame.Rect((self.pos[0] + 2, self.pos[1] + 2),\
+                                                                   (self.size[0] - 4, self.size[1] - 4)))
+        text_size = self.size[1]*4//5
+        if len(self.__text) != 0:
+            draw_text(self.__text, text_size, colors['black'], self.window,\
+                      (self.pos[0] + self.size[0]/2, self.pos[1] + self.size[1]/2))
+        else:
+            draw_text(self.__prompt_text, text_size, colors['black'], self.window,\
+                      (self.pos[0] + self.size[0]/2, self.pos[1] + self.size[1]/2))
+            
+        
+    def handle_event(self, event):
+        if event.type == MOUSEBUTTONDOWN:
+            borders = (self.pos[0], self.pos[1], self.pos[0] + self.size[0], self.pos[1] + self.size[1])
+            if borders[0] <= event.pos[0] <= borders[2] and borders[1] <= event.pos[1] <= borders[3]:
+                self.__is_active = True
+        if event.type == KEYDOWN:
+            if event.unicode in string.ascii_letters + string.digits and len(self.__text) < 16:
+                self.__text += event.unicode
+            if event.key == K_BACKSPACE and len(self.__text) > 0:
+                self.__text = self.__text[:-1]
         
         
         
